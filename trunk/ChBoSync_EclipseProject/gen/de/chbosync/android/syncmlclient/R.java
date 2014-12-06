789cfca180063227a32f4bdbd720292e3773ecb4
mdecker.androapps <mdecker.androapps@6171ee56-cc8b-3376-6052-e493bbf04b8d>
2014-12-06 21:11:19 +0000

    Added label "Powered by ..." at the top of tab for sync settings.

--- trunk/ChBoSync_EclipseProject/gen/de/chbosync/android/syncmlclient/R.java
+++ trunk/ChBoSync_EclipseProject/gen/de/chbosync/android/syncmlclient/R.java
@@ -192,8 +192,9 @@ public final class R {
         public static final int homescreen=0x7f030004;
         public static final int homescreen_single=0x7f030005;
         public static final int login_screen=0x7f030006;
-        public static final int settings=0x7f030007;
-        public static final int two_lines_checkbox=0x7f030008;
+        public static final int powered_by_textview=0x7f030007;
+        public static final int settings=0x7f030008;
+        public static final int two_lines_checkbox=0x7f030009;
     }
     public static final class string {
         public static final int about_label=0x7f0500b2;
